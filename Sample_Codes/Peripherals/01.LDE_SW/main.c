0494ef18f50483f8940560fe45c8deb66940aca3
LogsunSystems <logsunsystems@gmail.com>
2026-03-11 15:54:44 +0530

    Basic Project Structure Created

---
+++ Sample_Codes/Peripherals/01.LDE_SW/main.c
@@ -0,0 +1,26 @@
+/**
+ * Title: LED_Switch
+ * Brief: The LED will gets ON/OFF wwhen Respective Button gets Operated
+ * Company: Logsun Systems
+ * Date: 11-03-2026
+ * Author: Shreyas Deshpande
+ *
+ * -------------------------------------------------------------------------
+ *                      Connections
+ * -------------------------------------------------------------------------
+ *
+ */
+#include <gpiod.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)
+{
+
+    while(1)
+    {
+        //Infinite Loop
+    }
+
+    return 0;
+}
