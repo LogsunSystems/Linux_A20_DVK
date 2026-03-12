9b802c4c47ac8f55faf08402308e45743837aaac
Shreyas Deshpande <shreyasdeshpande12@gmail.com>
2026-03-12 23:09:18 +0530

    Comments added

--- Sample_Codes/System_programming/Process_Creation/main.c
+++ Sample_Codes/System_programming/Process_Creation/main.c
@@ -10,33 +10,33 @@
  *
  */
 
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/wait.h>
+#include <stdio.h>// For printf() and perror()
+#include <unistd.h>// For fork() and execl()
+#include <sys/wait.h>// For waitpid()
 
 int main(void)
 {
-    pid_t p1, p2;
+    pid_t p1, p2;// Declare variables to store process IDs of child processes
 
     p1 = fork(); // Create Process-1
-    if (p1 == 0)
+    if (p1 == 0)// If fork() returns 0, we are in the child process
     {
         execl("./process_1", "process1", NULL); // Assign Executable file to the process-1
 
-        perror("Failed to execute process-1");
-        return 1;
+        perror("Failed to execute process-1");// If execl() returns, it means there was an error
+        return 1; // Exit with an error code
     }
 
-    p2 = fork();
-    if (p2 == 0)
+    p2 = fork();// Create Process-2
+    if (p2 == 0)// If fork() returns 0, we are in the child process        
     {
-        execl("./process_2", "proces2", NULL);
+        execl("./process_2", "proces2", NULL);// Assign Executable file to the process-2
 
-        perror("Failed to execute process-2");
-        return 1;
+        perror("Failed to execute process-2");// If execl() returns, it means there was an error
+        return 1;// Exit with an error code
     }
 
     waitpid(p1, NULL, 0); // Wait until process-1 gets finish it's execution.
-    waitpid(p1, NULL, 0);
-    return 0;
+    waitpid(p1, NULL, 0);// Wait until process-2 gets finish it's execution.
+    return 0;// Exit with success code
 }
