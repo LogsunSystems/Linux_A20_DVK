e53d028a06c9eb8f92cbc4e34b11244f11275341
Shreyas Deshpande <shreyasdeshpande12@gmail.com>
2026-03-12 22:20:38 +0530

    Process-2 structure added
    The main file gets complete here

--- Sample_Codes/System_programming/Process_Creation/main.c
+++ Sample_Codes/System_programming/Process_Creation/main.c
@@ -8,25 +8,35 @@
  * Date: 12-03-2026
  * Author: Shreyas Deshpande
  *
- * -------------------------------------------------------------------------
- *                      Connections
- * -------------------------------------------------------------------------
- *
  */
 
 #include <stdio.h>
 #include <unistd.h>
-#include<sys/wait.h>
+#include <sys/wait.h>
 
 int main(void)
 {
-    pid_t p1,p2;
+    pid_t p1, p2;
 
-    p1=fork(); //Create Process-1
-    if(p1==0){
-        execl("./process1","process1",NULL); //Assign Executable file to the process-1
+    p1 = fork(); // Create Process-1
+    if (p1 == 0)
+    {
+        execl("./process_1", "process1", NULL); // Assign Executable file to the process-1
+
+        perror("Failed to execute process-1");
+        return 1;
     }
-    
-    waitpid(p1,NULL,0);//Wait until process-1 gets finish it's execution.
+
+    p2 = fork();
+    if (p2 == 0)
+    {
+        execl("./process_2", "proces2", NULL);
+
+        perror("Failed to execute process-2");
+        return 1;
+    }
+
+    waitpid(p1, NULL, 0); // Wait until process-1 gets finish it's execution.
+    waitpid(p1, NULL, 0);
     return 0;
 }
